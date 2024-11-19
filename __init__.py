04e84bbee4518d318dd9d548cb95d50cc5808590
GibbonGambits <166489004+GibbonGambits@users.noreply.github.com>
2024-11-18 20:18:52 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,10 +7,23 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+# Set up logging
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+file_handler = RotatingFileHandler('logs/flaskwebproject.log', maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('FlaskWebProject startup')
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
